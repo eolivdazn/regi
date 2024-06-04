4fc5c371c084e23099cfbf38179b3def0bfc689a
Eduardo Oliveira <oliveiraed@ryanair.com>
2024-06-04 20:35:44 +0100

    test

--- test.js
+++ test.js
@@ -29,44 +29,44 @@ let config = {
 };
 // Book the class
 
-const buttonPattern = /id_aula=(.*?)'/g;
+const idAulaPattern = /id_aula=(.*?)'/g;
 axios.request(config)
 .then((response) => {
-      const matches = response.data.matchAll(buttonPattern);
+      const matches = response.data.matchAll(idAulaPattern);
 
  for (const match of matches) {
      aulaUrls.push(match[1]);
  }
  console.log(aulaUrls)
- let configBook = {
-   method: 'get',
-   maxBodyLength: Infinity,
-   //${aulaUrls[0] first class of the day
-   url: `https://www.regibox.pt/app/app_nova/php/aulas/marca_aulas.php?id_aula=${aulaUrls[9]}&z=${auth}`,
-   headers: {
-     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-     'accept-language': 'en-US,en;q=0.9,gl;q=0.8,pt;q=0.7',
-     'cookie': `PHPSESSID=8hakrm3tf9drq38ntd7fs4e2k4; regybox_boxes=%2A${auth}; regybox_user=${auth}; _ga=GA1.1.1171832173.1717503286; _ga_17R0Y8DFZK=GS1.1.1717502445.1.1.1717504553.0.0.0; _ga_M0FG753LRM=GS1.1.1717503286.1.1.1717505599.0.0.0; regybox_boxes=%2A${auth}; regybox_user=${auth}`,
-     'priority': 'u=0, i',
-     'referer': `https://www.regibox.pt/app/app_nova/index.php?z=${auth}&id=12&lang=en&tipo=direct&id_box=12&type=direct`,
-     'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
-     'sec-ch-ua-mobile': '?0',
-     'sec-ch-ua-platform': '"macOS"',
-     'sec-fetch-dest': 'iframe',
-     'sec-fetch-mode': 'navigate',
-     'sec-fetch-site': 'same-origin',
-     'sec-fetch-user': '?1',
-     'upgrade-insecure-requests': '1',
-     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
-   }
- };
- axios.request(configBook)
- .then((response) => {
-   console.log(JSON.stringify(response.data));
- })
- .catch((error) => {
-   console.log(error);
- });
+// let configBook = {
+//   method: 'get',
+//   maxBodyLength: Infinity,
+//   //${aulaUrls[0] first class of the day
+//   url: `https://www.regibox.pt/app/app_nova/php/aulas/marca_aulas.php?id_aula=${aulaUrls[9]}&z=${auth}`,
+//   headers: {
+//     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+//     'accept-language': 'en-US,en;q=0.9,gl;q=0.8,pt;q=0.7',
+//     'cookie': `PHPSESSID=8hakrm3tf9drq38ntd7fs4e2k4; regybox_boxes=%2A${auth}; regybox_user=${auth}; _ga=GA1.1.1171832173.1717503286; _ga_17R0Y8DFZK=GS1.1.1717502445.1.1.1717504553.0.0.0; _ga_M0FG753LRM=GS1.1.1717503286.1.1.1717505599.0.0.0; regybox_boxes=%2A${auth}; regybox_user=${auth}`,
+//     'priority': 'u=0, i',
+//     'referer': `https://www.regibox.pt/app/app_nova/index.php?z=${auth}&id=12&lang=en&tipo=direct&id_box=12&type=direct`,
+//     'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+//     'sec-ch-ua-mobile': '?0',
+//     'sec-ch-ua-platform': '"macOS"',
+//     'sec-fetch-dest': 'iframe',
+//     'sec-fetch-mode': 'navigate',
+//     'sec-fetch-site': 'same-origin',
+//     'sec-fetch-user': '?1',
+//     'upgrade-insecure-requests': '1',
+//     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
+//   }
+// };
+// axios.request(configBook)
+// .then((response) => {
+//   console.log(JSON.stringify(response.data));
+// })
+// .catch((error) => {
+//   console.log(error);
+// });
 
 })
 .catch((error) => {
